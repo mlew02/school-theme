0a621e013fac933e53f371c6dad551ee382fe94e
mlew02 <matthewjameslew@gmail.com>
2024-05-21 14:12:56 -0700

    added photo sizing and scroll

--- school-themes/template-parts/content-page.php
+++ school-themes/template-parts/content-page.php
@@ -11,12 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if (is_singular()):
-			the_title('<h1 class="entry-title">', '</h1>');
-		else:
-			the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-		endif;
-		?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<?php school_theme_post_thumbnail(); ?>
@@ -27,28 +22,28 @@
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . esc_html__('Pages:', 'school-theme'),
-				'after' => '</div>',
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'school-theme' ),
+				'after'  => '</div>',
 			)
 		);
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if (get_edit_post_link()): ?>
+	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
 			<?php
 			edit_post_link(
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__('Edit <span class="screen-reader-text">%s</span>', 'school-theme'),
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'school-theme' ),
 						array(
 							'span' => array(
 								'class' => array(),
 							),
 						)
 					),
-					wp_kses_post(get_the_title())
+					wp_kses_post( get_the_title() )
 				),
 				'<span class="edit-link">',
 				'</span>'
--- school-themes/template-parts/content.php
+++ school-themes/template-parts/content.php
@@ -33,11 +33,20 @@
 
 	<div class="entry-content">
 		<?php
-	if(is_single() ){
-		the_content();
-	}else{
-		the_excerpt();
-	}
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'school-theme' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				wp_kses_post( get_the_title() )
+			)
+		);
 
 		wp_link_pages(
 			array(
