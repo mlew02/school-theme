19a9cb18ef9451860bbeef1c2228991f76d649ed
mlew02 <matthewjameslew@gmail.com>
2024-05-28 22:00:48 -0700

    updated

--- page-schedule.php
+++ page-schedule.php
@@ -1,64 +1,53 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package School_Theme
- */
-
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <?php
+    while (have_posts()) :
+        the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+        get_template_part('template-parts/content', 'page');
 
-			// Check if ACF Pro plugin is active and if the schedule field exists
-if ( function_exists( 'get_field' ) ) {
-    $schedule = get_field('schedule');
+        // Check if ACF Pro plugin is active and if the schedule field exists
+        if (function_exists('get_field')) {
+            $schedule = get_field('schedule');
+            if ($schedule) :
+    ?>
+                <h2 class="head-sch" style="text-align:center;">Weekly Course Schedule</h2>
+                <div class="schedule">
+                    <table>
+                        <tr class="name">
+                            <th>Date</th>
+                            <th>Course</th>
+                            <th>Instructor</th>
+                        </tr>
 
-    if( $schedule ): ?>
-        <div class="schedule">
-            <table>
-                <tr>
-                    <th>Date</th>
-                    <th>Course</th>
-                    <th>Instructor</th>
-                </tr>
+                        <?php foreach ($schedule as $item) : ?>
+                            <tr>
+                                <td><?php echo isset($item['date']) ? esc_html($item['date']) : ''; ?></td>
+                                <td><?php echo isset($item['course']) ? esc_html($item['course']) : ''; ?></td>
+                                <td><?php echo isset($item['instructor']) ? esc_html($item['instructor']) : ''; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
 
-                <?php foreach( $schedule as $item ): ?>
-                    <tr>
-                        <td><?php echo isset($item['date']) ? esc_html( $item['date'] ) : ''; ?></td>
-                        <td><?php echo isset($item['course']) ? esc_html( $item['course'] ) : ''; ?></td>
-                        <td><?php echo isset($item['instructor']) ? esc_html( $item['instructor'] ) : ''; ?></td>
-                    </tr>
-                <?php endforeach; ?>
+                    </table>
+                </div>
+            <?php else : ?>
+                <p>No schedule found.</p>
+        <?php
+            endif;
+        } else {
+            // ACF Pro plugin is not active
+            echo "<p>Advanced Custom Fields Pro plugin is not active.</p>";
+        }
 
-            </table>
-        </div>
-    <?php else: ?>
-        <p>No schedule found.</p>
-    <?php endif;
-} else {
-    // ACF Pro plugin is not active
-    echo "<p>Advanced Custom Fields Pro plugin is not active.</p>";
-}
+    endwhile; // End of the loop.
+    ?>
 
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
+?>
