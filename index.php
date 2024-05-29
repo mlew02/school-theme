cde0123954f162b6cfc930cc62b298cba11365c5
mlew02 <matthewjameslew@gmail.com>
2024-05-29 14:14:14 -0700

    most updated

--- index.php
+++ index.php
@@ -18,9 +18,9 @@ get_header();
 <main id="primary" class="site-main">
 
 	<?php
-	if ( have_posts() ) :
+	if (have_posts()):
 
-		if ( is_home() && ! is_front_page() ) :
+		if (is_home() && !is_front_page()):
 			?>
 			<header>
 				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
@@ -30,18 +30,21 @@ get_header();
 		?>
 
 		<section class="news">
-			<h2>Recent News</h2>
+			<h2 style="font-size:2.5rem;">Recent News</h2>
 			<div class="news-container">
 				<?php
 				// Start the Loop
-				while ( have_posts() ) :
+				while (have_posts()):
 					the_post();
 					?>
 					<article class="news-post" data-aos="fade-up">
 						<a href="<?php the_permalink(); ?>">
-							<?php the_post_thumbnail('portrait-student'); ?>
 							<h3><?php the_title(); ?></h3>
 						</a>
+						<p class="post-meta">Posted by <a><?php the_author(); ?></a> on <a><?php echo get_the_date(); ?></a></p>
+						<?php the_post_thumbnail('portrait-blog'); ?>
+						<?php echo "<p>" . wp_trim_words(get_the_content(), 30); "</p>" ?>
+						<p>Posted in: <?php the_category(', '); ?> Tagged: <?php the_tags('', ', ', ''); ?></p> <p><a href="<?php comments_link(); ?>">Leave a Comment</a></p>
 					</article>
 					<?php
 				endwhile;
@@ -52,9 +55,9 @@ get_header();
 
 		the_posts_navigation();
 
-	else :
+	else:
 
-		get_template_part( 'template-parts/content', 'none' );
+		get_template_part('template-parts/content', 'none');
 
 	endif;
 	?>
